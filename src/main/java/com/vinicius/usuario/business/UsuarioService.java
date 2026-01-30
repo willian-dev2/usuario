e6424d268e1f2c9018dbc96d6d6db048e1fb45d7
willian <viniciussousaitb35@gmail.com>
2026-01-30 15:58:41 -0300

    ajuste para funcionamento do método Post

--- src/main/java/com/vinicius/usuario/business/UsuarioService.java
+++ src/main/java/com/vinicius/usuario/business/UsuarioService.java
@@ -3,8 +3,11 @@ package com.vinicius.usuario.business;
 import com.vinicius.usuario.business.converter.UsuarioConverter;
 import com.vinicius.usuario.business.dto.UsuarioDTO;
 import com.vinicius.usuario.infraestructure.entity.Usuario;
+import com.vinicius.usuario.infraestructure.exceptions.ConflictException;
 import com.vinicius.usuario.infraestructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,14 +16,31 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
+    private final PasswordEncoder passwordEncoder;
 
 
     public UsuarioDTO salvarUsuario(UsuarioDTO usuarioDTO) {
+        emailExiste(usuarioDTO.getEmail());
+        usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
         Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
         return usuarioConverter.paraUsuarioDTO(
                 usuarioRepository.save(usuario)
         );
     }
 
+    public void emailExiste(String email) {
+        try {
+            boolean existe = verificaEmailExistente(email);
+            if (existe) {
+                throw new ConflictException("Email já cadastrado " + email);
+            }
+        } catch (ConflictException e) {
+            throw new ConflictException("Email já cadastrado ", e.getCause());
+        }
+    }
+
+    public boolean verificaEmailExistente(String email) {
+        return usuarioRepository.existsByEmail(email);
+    }
 
 }
