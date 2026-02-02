e5286fc194c5e095ab515372b5c7729111551718
willian <viniciussousaitb35@gmail.com>
2026-02-02 01:21:48 -0300

    adicionados metodos de gravar endereco e telefone do usuario

--- src/main/java/com/vinicius/usuario/controller/UsuarioController.java
+++ src/main/java/com/vinicius/usuario/controller/UsuarioController.java
@@ -70,4 +70,21 @@ public class UsuarioController {
 
         return ResponseEntity.ok(usuarioService.atualizaTelefone(id, dto));
     }
+
+    @PostMapping("/endereco")
+    public ResponseEntity<EnderecoDTO> cadastraEndereco(@RequestBody EnderecoDTO dto,
+                                                        @RequestHeader("Authorization") String token) {
+
+        return ResponseEntity.ok(usuarioService.cadastraEndereco(token, dto));
+
+    }
+
+    @PostMapping("/telefone")
+    public ResponseEntity<TelefoneDTO> cadastraTelefone(@RequestBody TelefoneDTO dto,
+                                                        @RequestHeader("Authorization") String token) {
+
+        return ResponseEntity.ok(usuarioService.cadastraTelefone(token, dto));
+
+    }
+
 }
